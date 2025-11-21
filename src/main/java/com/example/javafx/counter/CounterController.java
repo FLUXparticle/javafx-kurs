8083890c19376e3633318e34af0d12e13965d09e
Sven Reinck <sreinck@fluxparticle.de>
2025-11-21 05:09:15 +0100

    Counter mit Task

--- src/main/java/com/example/javafx/counter/CounterController.java
+++ src/main/java/com/example/javafx/counter/CounterController.java
@@ -1,6 +1,6 @@
 package com.example.javafx.counter;
 
-import javafx.application.*;
+import javafx.concurrent.*;
 import javafx.event.*;
 import javafx.scene.*;
 
@@ -18,20 +18,30 @@ public class CounterController {
     }
 
     private void startCounting(ActionEvent event) {
-        Thread worker = new Thread(() -> {
-            for (int i = 1; i <= 100; i++) {
-                try { Thread.sleep(50); } catch (Exception ignored) {}
+        var task = new CounterTask();
 
-                System.out.println(Thread.currentThread().getName() + ": " + i);
-                int value = i;
-                Platform.runLater(() -> {
-                    view.textField.setText(String.valueOf(value));
-                    view.progressBar.setProgress(value / 100.0);
-                });
+        view.textField.textProperty().bind(task.messageProperty());
+        view.progressBar.progressProperty().bind(task.progressProperty());
+
+        new Thread(task).start();
+    }
+
+    private static class CounterTask extends Task<Void> {
+        @Override
+        protected Void call() {
+            try {
+                for (int i = 1; i <= 100; i++) {
+                    Thread.sleep(50);
+                    System.out.println(Thread.currentThread().getName() + ": " + i);
+                    updateMessage(String.valueOf(i));
+                    updateProgress(i, 100);
+                    if (Thread.interrupted()) throw new InterruptedException();
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted!");
             }
-        });
-
-        worker.start();
+            return null;
+        }
     }
 
 }
